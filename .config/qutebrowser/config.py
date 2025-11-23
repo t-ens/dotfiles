faa2dc2ce90ab3a5f28992956c44cb0246247b3d
Travis Ens <ens.travis@gmail.com>
2025-11-23 10:04:14 -0500

    Add bluetooth headset battery to waybar

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,13 +1,11 @@
 config.load_autoconfig(False)
 
-# Adblocking
 config.set(
     'content.blocking.hosts.lists',
     [
         'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts',
     ]
 )
-
 config.set(
     'content.blocking.adblock.lists',
     [
@@ -36,19 +34,6 @@ search_engines = {
 }
 config.set('url.searchengines', search_engines)
 
-# Hinting
-config.set('hints.mode', 'word')
-config.set('hints.auto_follow_timeout', 200)
-
-# Use MPV for videos
-config.bind(',m', 'spawn mpv --title="youtube-player" {url}')
-config.bind(',M', 'hint links spawn mpv {hint-url}')
-config.bind(';M', 'hint --rapid links spawn mpv --title="youtube-player" {hint-url}')
-
-config.bind(',p', 'spawn --userscript to-playlist {url}')
-config.bind(',P', 'hint links spawn --userscript to-playlist {hint-url}')
-config.bind(';P', 'hint --rapid links spawn --userscript to-playlist {hint-url}')
-
 # Theming
 config.set('colors.webpage.preferred_color_scheme', 'dark')
 config.set('colors.webpage.darkmode.algorithm', 'lightness-cielab')
@@ -58,6 +43,11 @@ config.set('colors.webpage.darkmode.policy.images', 'smart')
 config.set('colors.webpage.darkmode.policy.page', 'smart')
 config.set('colors.webpage.darkmode.threshold.background', 0)
 config.set('colors.webpage.darkmode.threshold.foreground', 256)
+
+# Use MPV for videos
+config.bind(',m', 'spawn umpv {url}')
+config.bind(',M', 'hint links spawn umpv {hint-url}')
+config.bind(';M', 'hint --rapid links spawn umpv {hint-url}')
 
 # Aliases
 c.aliases['proxy-mitm'] = 'set content.proxy http://127.0.0.1:8080/'
